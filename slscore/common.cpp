5c42bdd06474d0765e659e25162cea9571d19c68
laurfb <laurfb@gmail.com>
2023-02-12 11:38:41 +0200

    Update common.cpp

--- slscore/common.cpp
+++ slscore/common.cpp
@@ -40,11 +40,13 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+
+#include <algorithm>
 #include <string>
 #include <vector>
-#include "common.hpp"
 
-#include <ctime>
+
+#include "common.hpp"
 
 
 /**
@@ -439,12 +441,15 @@ void sls_split_string(std::string str, std::string separator, std::vector<std::s
 	ADD_VECTOR_END(result, str.substr(lastPosition, string::npos));
 }
 
-std::string sls_find_string(std::vector<std::string> &src, std::string &dst)
+std::string sls_find_string(std::vector<std::string> &src, std::string &dst, bool caseSensitive)
 {
+    if (!caseSensitive) std::transform(dst.begin(), dst.end(), dst.begin(), ::tolower);
+
 	std::string ret = std::string("");
 	std::vector<std::string>::iterator it;
     for(it=src.begin(); it!=src.end();) {
     	std::string str = *it;
+        if (!caseSensitive) std::transform(str.begin(), str.end(), str.begin(), ::tolower);
     	it ++;
     	string::size_type pos = str.find(dst);
     	if (pos != std::string::npos)
@@ -826,4 +831,3 @@ void sls_init_ts_info(ts_info *ti)
         }
     }
 }
-
